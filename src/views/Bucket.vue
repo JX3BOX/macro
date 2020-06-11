8d1faed59a5a922370754dff0774947fd82bb564
iRuxu <rx6@qq.com>
2020-06-11 18:47:27 +0800

    add

---
+++ src/views/Bucket.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="m-bucket">
+        <el-alert title="即将推出，敬请期待" type="warning" show-icon> </el-alert>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Bucket",
+    props: [],
+    data: function() {
+        return {};
+    },
+    computed: {},
+    methods: {},
+    mounted: function() {},
+    components: {},
+};
+</script>
+
+<style lang="less">
+@import "../assets/css/bucket.less";
+</style>
