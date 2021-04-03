84a9fa87bf548fb37bba3628d4a70431206b8b72
iRuxu <rx6@qq.com>
2021-04-03 20:07:30 +0800

    cms2.0

---
+++ src/Post.vue
@@ -0,0 +1,52 @@
+<template>
+    <div id="app">
+        <Header></Header>
+        <Breadcrumb
+            name="云端宏"
+            slug="macro"
+            root="/macro"
+            :publishEnable="true"
+            :adminEnable="true"
+            :feedbackEnable="true"
+            :crumbEnable="true"
+        >
+            <img slot="logo" svg-inline src="./assets/img/macro.svg" />
+        </Breadcrumb>
+        <LeftSidebar>
+            <Nav class="m-nav" />
+        </LeftSidebar>
+        <Main :withoutRight="false">
+            <single />
+            <RightSidebar>
+                <Side class="m-extend" />
+            </RightSidebar>
+            <Footer></Footer>
+        </Main>
+    </div>
+</template>
+
+<script>
+import Nav from "@/components/single_nav.vue";
+import Side from "@/components/single_side.vue";
+import single from "@/components/single.vue";
+export default {
+    name: "App",
+    props: [],
+    data: function () {
+        return {};
+    },
+    methods: {},
+    components: {
+        Nav,
+        Side,
+        single,
+    },
+    beforeCreate : function (){
+        
+    }
+};
+</script>
+
+<style lang="less">
+@import "./assets/css/app.less";
+</style>
---
+++ src/post.js
@@ -0,0 +1,27 @@
+Vue.config.productionTip = false;
+
+// 第三方UI组件
+import Vue from "vue";
+import ElementUI from "element-ui";
+Vue.use(ElementUI);
+import VueClipboard from 'vue-clipboard2'
+Vue.use(VueClipboard)
+
+
+// 通用UI模块
+import JX3BOX_UI from '@jx3box/jx3box-common-ui'
+import "@jx3box/jx3box-common/css/element.css";
+import "@jx3box/jx3box-common/css/normalize.css";
+Vue.use(JX3BOX_UI);
+import Comments from "@jx3box/jx3box-comment-ui"
+Vue.use(Comments)
+
+// 数据与路由
+import store from "@/store";
+
+import App from "./Post.vue";
+new Vue({
+    store,
+    render: h => h(App),
+}).$mount("#app");
+
