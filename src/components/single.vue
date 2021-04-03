242466517101711014f77ac3357c2593588c3220
iRuxu <rx6@qq.com>
2021-04-03 21:06:54 +0800

    update

--- src/components/single.vue
+++ src/components/single.vue
@@ -4,23 +4,17 @@
             <div class="u-meta u-sub-block">
                 <em class="u-label">心法</em>
                 <span class="u-value">
-                    <img
-                        class="u-icon-xf"
-                        :src="xficon_id | xficon"
-                        :alt="xf"
-                    />
+                    <img class="u-icon-xf" :src="xficon_id | xficon" :alt="xf" />
                     {{ xf }}
                 </span>
             </div>
             <div class="u-meta u-sub-block">
                 <em class="u-label">资料片</em>
-                <span class="u-value">
-                    {{ post.zlp || post.meta_1 || '未知'}}
-                </span>
+                <span class="u-value">{{ post.zlp || post.meta_1 || '未知'}}</span>
             </div>
         </template>
         <!-- 宏内容 -->
-        <div class="m-single-macro">
+        <div class="m-single-macro" v-if="visible">
             <el-tabs v-model="active" type="card">
                 <el-tab-pane v-for="(item, i) in data" :key="i" :name="i + ''">
                     <!-- tab -->
@@ -29,25 +23,22 @@
                         <b>{{ item.name }}</b>
                     </span>
                     <!-- 宏 -->
-                    <el-divider content-position="left" v-if="item.macro"
-                        >宏</el-divider
-                    >
+                    <el-divider content-position="left" v-if="item.macro">宏</el-divider>
                     <div class="u-usage" v-if="item.desc">{{ item.desc }}</div>
                     <div
                         class="u-macro macro-box"
                         :class="{ withUsage: item.desc }"
                         v-if="item.macro"
                     >
-                        <macro :ctx="item.macro" :lang="lang" :name="post.author.name + '#' + item.name"/>
+                        <macro
+                            :ctx="item.macro"
+                            :lang="lang"
+                            :name="post.author.name + '#' + item.name"
+                        />
                     </div>
                     <!-- 奇穴 -->
-                    <el-divider content-position="left" v-if="item.talent"
-                        >奇穴</el-divider
-                    >
-                    <div
-                        class="u-talent talent-box"
-                        :id="`talent-box-${i}`"
-                    ></div>
+                    <el-divider content-position="left" v-if="item.talent">奇穴</el-divider>
+                    <div class="u-talent talent-box" :id="`talent-box-${i}`"></div>
                     <div class="u-panel u-talent-panel" v-if="item.talent">
                         <el-button
                             class="u-talent-panel-copycode"
@@ -57,8 +48,7 @@
                             v-clipboard:copy="item.talent"
                             v-clipboard:success="onCopy"
                             v-clipboard:error="onError"
-                            >复制奇穴编码</el-button
-                        >
+                        >复制奇穴编码</el-button>
                         <el-button
                             class="u-talent-panel-copytxt"
                             icon="el-icon-document-copy"
@@ -67,31 +57,25 @@
                             v-clipboard:copy="getTalentTXT(i)"
                             v-clipboard:success="onCopy"
                             v-clipboard:error="onError"
-                            >复制奇穴文字</el-button
-                        >
+                        >复制奇穴文字</el-button>
                         <el-button
-                        class="u-talent-panel-copysq"
+                            class="u-talent-panel-copysq"
                             icon="el-icon-scissors"
                             plain
                             size="mini"
                             v-clipboard:copy="getTalentSQ(item.talent)"
                             v-clipboard:success="onCopy"
                             v-clipboard:error="onError"
-                            >复制奇穴序列</el-button
-                        >
+                        >复制奇穴序列</el-button>
                     </div>
                     <!-- 配装 -->
                     <el-divider content-position="left" v-if="item.equip && item.equip_type">配装方案</el-divider>
                     <div class="u-equipbox">
-                        <Equip :id="item.equip" v-if="item.equip_type == 'jx3box'"/>
+                        <Equip :id="item.equip" v-if="item.equip_type == 'jx3box'" />
                     </div>
                     <!-- 急速 -->
-                    <el-divider content-position="left" v-if="item.speed"
-                        >推荐急速</el-divider
-                    >
-                    <div class="u-speed" v-if="item.speed">
-                        {{ item.speed }}
-                    </div>
+                    <el-divider content-position="left" v-if="item.speed">推荐急速</el-divider>
+                    <div class="u-speed" v-if="item.speed">{{ item.speed }}</div>
                 </el-tab-pane>
             </el-tabs>
         </div>
@@ -99,7 +83,12 @@
 </template>
 
 <script>
-import {__ossRoot,__ossMirror,__iconPath,__imgPath} from '@jx3box/jx3box-common/data/jx3box.json'
+import {
+    __ossRoot,
+    __ossMirror,
+    __iconPath,
+    __imgPath,
+} from "@jx3box/jx3box-common/data/jx3box.json";
 import singlebox from "@jx3box/jx3box-page/src/cms-single";
 import { getPost } from "../service/post.js";
 import { getStat, postStat } from "@jx3box/jx3box-common/js/stat";
@@ -107,18 +96,18 @@ import { getStat, postStat } from "@jx3box/jx3box-common/js/stat";
 import macro from "@/components/macro.vue";
 import xfmap from "@jx3box/jx3box-data/data/xf/xf.json";
 import talent from "@jx3box/jx3box-talent";
-import Equip from '@jx3box/jx3box-editor/src/Equip.vue'
+import Equip from "@jx3box/jx3box-editor/src/Equip.vue";
 
 export default {
     name: "single",
     props: [],
-    data: function() {
+    data: function () {
         return {
             loading: false,
 
             post: {},
             stat: {},
-            lang : 'cn',
+            lang: "cn",
 
             data: [],
 
@@ -127,31 +116,34 @@ export default {
         };
     },
     computed: {
-        id: function() {
+        id: function () {
             return this.$store.state.id;
         },
-        xf: function() {
+        xf: function () {
             return _.get(this.post, "post_subtype");
         },
-        xficon_id: function() {
+        xficon_id: function () {
             return this.xf && xfmap[this.xf]["id"];
+        },
+        visible : function (){
+            return this.post._check
         },
     },
     methods: {
-        onCopy: function(val) {
+        onCopy: function (val) {
             this.$notify({
                 title: "复制成功",
                 message: "复制内容 : " + val.text,
                 type: "success",
             });
         },
-        onError: function() {
+        onError: function () {
             this.$notify.error({
                 title: "复制失败",
                 message: "请手动复制",
             });
         },
-        getTalentSQ: function(talent) {
+        getTalentSQ: function (talent) {
             if (talent) {
                 try {
                     let _parsed = JSON.parse(talent);
@@ -163,29 +155,29 @@ export default {
                 return "";
             }
         },
-        getTalentTXT: function(i) {
+        getTalentTXT: function (i) {
             return this.talents[i];
         },
     },
     filters: {
-        xficon: function(val) {
+        xficon: function (val) {
             return __imgPath + "image/xf/" + val + ".png";
         },
-        iconURL: function(val) {
+        iconURL: function (val) {
             return __iconPath + "icon/" + val + ".png";
         },
     },
-    created: function() {
+    created: function () {
         if (this.id) {
             this.loading = true;
             getPost(this.id)
                 .then((res) => {
                     this.post = this.$store.state.post = res.data.data;
-                    this.data = this.post.post_meta.data
-                    this.$store.state.user_id = this.post.post_author
+                    this.data = this.post.post_meta && this.post.post_meta.data;
+                    this.$store.state.user_id = this.post.post_author;
                 })
                 .then(() => {
-                    if (this.data.length) {
+                    if (this.data && this.data.length) {
                         this.data.forEach((item, i) => {
                             let container = `#talent-box-${i}`;
                             let schema = item.talent;
@@ -213,16 +205,16 @@ export default {
                     this.loading = false;
                 });
 
-            getStat('macro',this.id).then((res) => {
+            getStat("macro", this.id).then((res) => {
                 this.stat = res.data;
             });
-            postStat('macro',this.id);
+            postStat("macro", this.id);
         }
     },
     components: {
         macro,
         singlebox,
-        Equip
+        Equip,
     },
 };
 </script>
