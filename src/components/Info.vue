a49e5920fa6cdf72064442a5f361bbe512facec1
iRuxu <rx6@qq.com>
2021-12-03 01:51:45 +0800

    feat:add mobile router:

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -4,8 +4,11 @@
             <!-- <span class="u-item" href="" target="_blank">🌈 自用宏将会被设为私有状态,不影响游戏内加载</span> -->
         </div>
 
-        <div class="m-info-router" v-if="mode == 'list'">
+        <div class="m-info-router">
             <router-link title="云端宏" to="/"><i class="el-icon-collection"></i></router-link>
+            <router-link title="我的宏" to="/bucket"><i class="el-icon-receiving"></i></router-link>
+            <router-link title="收藏夹" to="/fav"><i class="el-icon-star-off"></i></router-link>
+            <router-link title="亲友宏" to="/landspace"><i class="el-icon-sugar"></i></router-link>
             <router-link title="排行榜" to="/rank"><i class="el-icon-s-data"></i></router-link>
         </div>
     </div>
@@ -19,9 +22,6 @@ export default {
         return {};
     },
     computed: {
-        mode: function() {
-            return this.$store.state.mode;
-        },
     },
     methods: {},
     mounted: function() {}
