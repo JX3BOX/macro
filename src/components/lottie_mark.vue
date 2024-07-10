42b2a4f0592631283cb30e4345f8b5cec8e48748
iRuxu <rx6@qq.com>
2024-07-10 17:47:07 +0800

    feat:lottie mark

---
+++ src/components/lottie_mark.vue
@@ -0,0 +1,42 @@
+<!-- src/components/LottieAnimation.vue -->
+<template>
+    <div ref="lottieContainer" :style="{ width: width, height: height }"></div>
+</template>
+
+<script>
+import lottie from "lottie-web";
+const animations = require.context("../assets/lottie", false, /\.json$/);
+
+export default {
+    props: {
+        mark: {
+            type: String,
+            default: "star",
+        },
+        width: {
+            type: String,
+            default: "24px",
+        },
+        height: {
+            type: String,
+            default: "24px",
+        },
+    },
+    mounted() {
+        this.loadAnimation();
+    },
+    methods: {
+        loadAnimation() {
+            lottie.loadAnimation({
+                container: this.$refs.lottieContainer,
+                renderer: "svg",
+                loop: true,
+                autoplay: true,
+                animationData: animations(`./${this.mark}.json`),
+            });
+        },
+    },
+};
+</script>
+
+<style scoped></style>
